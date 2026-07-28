a35bfdad75b17f4481420d37a40d5e52ce09c77d
squid <raspberryratman@gmail.com>
2026-07-28 15:51:34 -0600

    working on adding flags handling

--- cat.c
+++ cat.c
@@ -8,15 +8,35 @@ FILE * open_file(char *filepath);
 
 int main(int argc, char *argv[]) {
   int exit_status = 0;
+  int i;
+
+
+  // will contain CLI flags
+  typedef struct {
+    int number_lines;
+    int show_all;
+  }
+
   // needs to get number of arguments if not 2 then reject
   if (argc == 1) {
     print_file(stdin);
     return exit_status;
   }
 
-  // opens each file path and diplays them one after another
+  // parse any flags
+  for (i = 1; i < argc; i++) {
+    if (strcmp(argv[i][0], '-') == 0) {
+      if ((strcmp(argv[i], "-n") == 0) || ((strcmp(argv[i], "--number") == 0))) {
+        number = 1;
+      }
+      if ((strcmp(argv[i], "-A") == 0) || ((strcmp(argv[i], "--show-all") == 0))) {
+        show_all = 1;
+    }
+  }
+
+  // opens each file path and displays them one after another
   FILE *fp;
-  for (int i = 1; i < argc; i++) {
+  for (i = 1; i < argc; i++) {
     // if argument is "-" takes input from stdin until EOF (ctrl+D) then continues  to next argument
     if (strcmp("-", argv[i]) == 0) {
       print_file(stdin); 
