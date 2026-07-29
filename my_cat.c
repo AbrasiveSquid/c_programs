1461aa8fab13b58baf43558225d1cd3feb00e71c
squid <raspberryratman@gmail.com>
2026-07-29 13:14:51 -0600

    refactored parse flags to a method

--- my_cat.c
+++ my_cat.c
@@ -13,11 +13,11 @@ typedef struct {
 void print_file(FILE *fp, Options * flag, int * line_ptr);
 FILE * open_file(char *filepath);
 Options * init_options(void);
+void parse_flags(char *argv[], int size, Options *flags); 
 
 
 int main(int argc, char *argv[]) {
   int exit_status = 0;
-  int i;
   int line = 1; 
   int * line_ptr = &line;
   
@@ -25,27 +25,16 @@ int main(int argc, char *argv[]) {
   if (flags == NULL) {
     return 1;
   }
+  parse_flags(argv, argc, flags);
 
   if (argc == 1) {
     print_file(stdin, flags, line_ptr);
     return exit_status;
   }
 
-  // parse any flags
-  for (i = 1; i < argc; i++) {
-    if (argv[i][0] == '-') {
-      if ((strcmp(argv[i], "-n") == 0) || ((strcmp(argv[i], "--number") == 0))) {
-        flags->number_lines = true;
-      }
-      if ((strcmp(argv[i], "-A") == 0) || ((strcmp(argv[i], "--show-all") == 0))) {
-        flags->show_all = true;
-      }
-    }
-  }
-
   // opens each file path and displays them one after another
   FILE *fp;
-  for (i = 1; i < argc; i++) {
+  for (int i = 1; i < argc; i++) {
     // if argument is "-" takes input from stdin until EOF (ctrl+D) then continues  to next argument
     if (strcmp("-", argv[i]) == 0) {
       print_file(stdin, flags, line_ptr); 
@@ -107,3 +96,16 @@ Options * init_options(void) {
   return flags;
   }
 
+// goes through the input and sets and flag in the CL args
+void parse_flags(char *argv[], int size, Options *flags) {
+  for (int i = 1; i < size; i++) {
+    if (argv[i][0] == '-') {
+      if ((strcmp(argv[i], "-n") == 0) || ((strcmp(argv[i], "--number") == 0))) {
+        flags->number_lines = true;
+      }
+      if ((strcmp(argv[i], "-A") == 0) || ((strcmp(argv[i], "--show-all") == 0))) {
+        flags->show_all = true;
+      }
+    }
+  }
+}
